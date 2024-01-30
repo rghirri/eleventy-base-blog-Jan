3e6551944da04eb0c9a5978058f757bc43dfc63b
rghirri <57195308+rghirri@users.noreply.github.com>
2024-01-30 19:47:32 +0000

    tasks complete

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,10 +2,10 @@ module.exports = {
 	title: "Eleventy Base Blog v8",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences as a naval navel-gazer. test",
 	author: {
 		name: "Your Name Here",
 		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		url: "https://example.com/about-me/",
+	},
+};
